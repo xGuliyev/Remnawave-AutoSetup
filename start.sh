29a9601deccb75f9b65d433e18ebf5fdca64bc27
Фамиль Гулиев <99288895+xGuliyev@users.noreply.github.com>
2025-12-31 00:31:21 +0300

    Update Remnawave installer download URL

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Downloading and preparing Remnawave installer..."
-wget -O install_temp.sh https://raw.githubusercontent.com/AsanFillter/Remnawave-AutoSetup/main/install_remnawave.sh
+wget -O install_temp.sh https://raw.githubusercontent.com/xGuliyev/Remnawave-AutoSetup/main/install_remnawave.sh
 sed -i 's/\r$//' install_temp.sh
 mv install_temp.sh install_remnawave.sh
 chmod +x install_remnawave.sh
